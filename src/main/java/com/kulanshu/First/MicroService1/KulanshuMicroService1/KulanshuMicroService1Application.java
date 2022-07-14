b10cf9b8af15ead826d7b89c1d2a2d4532a74dae
Kulanshu Sharma <kulanshusharma@Kulanshus-MacBook-Air.local>
2022-07-14 10:01:58 +0530

    Adding Project

---
+++ src/main/java/com/kulanshu/First/MicroService1/KulanshuMicroService1/KulanshuMicroService1Application.java
@@ -0,0 +1,13 @@
+package com.kulanshu.First.MicroService1.KulanshuMicroService1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KulanshuMicroService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KulanshuMicroService1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kulanshu/First/MicroService1/KulanshuMicroService1/KulanshuMicroService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kulanshu.First.MicroService1.KulanshuMicroService1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KulanshuMicroService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
